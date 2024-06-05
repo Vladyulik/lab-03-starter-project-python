21fed34fbe7e1b0f9103980e6c9a605600bfd56f
vladyulik <vanya123wow321@gmail.com>
2024-06-05 03:55:51 +0300

    Add new endpoint for matrices

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
-
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/multiply_matrices')
+def multiply_matrices() -> dict:
+    matrix_a = np.random.randint(0, 100, (10, 10)).tolist()
+    matrix_b = np.random.randint(0, 100, (10, 10)).tolist()
+    
+    product = np.dot(matrix_a, matrix_b).tolist()
+    
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
+    }
